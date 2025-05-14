68cc62f01691975e227465a1b2e5db5470d76f56
upadhyayvaibhav229 <upadhyayvaibhav229@gmail.com>
2025-05-14 18:08:11 +0530

    Add Font Awesome and Bootstrap Icons; implement ShoppingCartIcon in Hero component

--- AssigementProject/src/Components/Hero.jsx
+++ AssigementProject/src/Components/Hero.jsx
@@ -4,6 +4,7 @@ import Banner from './Banner'
 import Bussiness from './Bussiness'
 import SuccessStories from './SuccessStories'
 import CountUp from 'react-countup'
+import { ShoppingCartIcon } from '@heroicons/react/24/solid'
 
 const Hero = () => {
     // style for bg image
@@ -93,6 +94,20 @@ const Hero = () => {
             {/* success stories */}
             <div>
                 <SuccessStories />
+            </div>
+
+            <div className='fixed right-5 bottom-5 flex flex-col gap-5'>
+                <button className='bg-red-600 text-white py-2 px-8 rounded-full'>
+                    <ShoppingCartIcon className='inline-block w-6 h-6' />
+
+                    Start Selling
+                </button>
+                <a href="#" className='bg-red-600 text-white py-2 px-8 rounded-full flex items-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+                    </svg>
+                    Customer Service
+                </a>
             </div>
         </>
     )
